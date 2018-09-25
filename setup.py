f5e3458cbceac0d6b3a7b25077444aadc0ae20ec
Kerrick Staley <kerrick@kerrickstaley.com>
2018-09-24 22:01:32 -0700

    Version 0.4.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='chineseflashcards',
-      version='0.4.3',
+      version='0.4.4',
       description='Create Chinese flashcards for Anki using the genanki lib',
       url='http://github.com/kerrickstaley/chineseflashcards',
       author='Kerrick Staley',
